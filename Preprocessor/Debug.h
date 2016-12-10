69991e9af78e5826eb24b03c9cd24e3ecd3ba313
dmsovetov <dmsovetov@gmail.com>
2016-12-10 23:23:58 +0200

    Added: simicolon to NIMBLE_BREAK macro on Windows

--- Preprocessor/Debug.h
+++ Preprocessor/Debug.h
@@ -45,7 +45,7 @@
 #if defined( NIMBLE_DEBUG )
     #if defined( NIMBLE_PLATFORM_WINDOWS )
         #include <crtdbg.h>
-        #define NIMBLE_BREAK        _CrtDbgBreak()
+        #define NIMBLE_BREAK        _CrtDbgBreak();
         #define NIMBLE_CHECK_MEMORY NIMBLE_ABORT_IF( _CrtCheckMemory() == FALSE, "memory corruption detected" )
     #elif defined( NIMBLE_PLATFORM_IOS ) || defined( DC_PLATFORM_MACOS )
         #if defined __arm__ || defined __thumb__
