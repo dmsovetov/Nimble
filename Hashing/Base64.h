1a316a147ae36efc645db622277f93f4afb427e3
Dmitry Sovetov <dmsovetov@gmail.com>
2016-12-13 16:27:51 +0200

    Fixed: compilation warning

--- Hashing/Base64.h
+++ Hashing/Base64.h
@@ -1,4 +1,4 @@
-﻿/* 
+/* 
 
    Copyright (C) 2004-2008 René Nyffenegger
 
@@ -31,112 +31,114 @@
 
 NIMBLE_BEGIN
 
-namespace Base64 {
-
     namespace Private {
-
+        
         //! All valid Base64 encoding characters
         static const String Base64Chars =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            "abcdefghijklmnopqrstuvwxyz"
-            "0123456789+/"
-            ;
-
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789+/"
+        ;
+        
         //! Returns true if a specified character is a valid Base64 character.
         static inline bool isBase64( u8 c )
         {
             return isalnum(c) || (c == '+') || (c == '/');
         }
-
+        
     } // namespace Private
 
-    //! Performs the Base64 encoding of an input array of bytes
-    static String encode( const u8* input, s32 length )
+    //! Base64 encoder and decoder.
+    class Base64
     {
-        std::string ret;
-        int i = 0;
-        int j = 0;
-        unsigned char char_array_3[3];
-        unsigned char char_array_4[4];
-
-        while (length--) {
-            char_array_3[i++] = *(input++);
-            if (i == 3) {
+    public:
+        
+        //! Performs the Base64 encoding of an input array of bytes
+        static String encode( const u8* input, s32 length )
+        {
+            std::string ret;
+            int i = 0;
+            int j = 0;
+            unsigned char char_array_3[3];
+            unsigned char char_array_4[4];
+            
+            while (length--) {
+                char_array_3[i++] = *(input++);
+                if (i == 3) {
+                    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+                    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+                    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+                    char_array_4[3] = char_array_3[2] & 0x3f;
+                    
+                    for(i = 0; (i <4) ; i++)
+                        ret += Private::Base64Chars[char_array_4[i]];
+                    i = 0;
+                }
+            }
+            
+            if (i)
+            {
+                for(j = i; j < 3; j++)
+                    char_array_3[j] = '\0';
+                
                 char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
                 char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
                 char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
                 char_array_4[3] = char_array_3[2] & 0x3f;
-
-                for(i = 0; (i <4) ; i++)
-                    ret += Private::Base64Chars[char_array_4[i]];
-                i = 0;
+                
+                for (j = 0; (j < i + 1); j++)
+                    ret += Private::Base64Chars[char_array_4[j]];
+                
+                while((i++ < 3))
+                    ret += '=';
             }
+            
+            return ret;
         }
-
-        if (i)
+        
+        //! Decodes the Base64-encoded data
+        static String decode( const String& input )
         {
-            for(j = i; j < 3; j++)
-                char_array_3[j] = '\0';
-
-            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-            char_array_4[3] = char_array_3[2] & 0x3f;
-
-            for (j = 0; (j < i + 1); j++)
-                ret += Private::Base64Chars[char_array_4[j]];
-
-            while((i++ < 3))
-                ret += '=';
-        }
-
-        return ret;
-    }
-
-    //! Decodes the Base64-encoded data
-    static String decode( const String& input )
-    {
-        int in_len = input.size();
-        int i = 0;
-        int j = 0;
-        int in_ = 0;
-        unsigned char char_array_4[4], char_array_3[3];
-        std::string ret;
-
-        while (in_len-- && ( input[in_] != '=') && Private::isBase64(input[in_])) {
-            char_array_4[i++] = input[in_]; in_++;
-            if (i ==4) {
-                for (i = 0; i <4; i++)
-                    char_array_4[i] = Private::Base64Chars.find(char_array_4[i]);
-
+            int in_len = input.size();
+            int i = 0;
+            int j = 0;
+            int in_ = 0;
+            unsigned char char_array_4[4], char_array_3[3];
+            std::string ret;
+            
+            while (in_len-- && ( input[in_] != '=') && Private::isBase64(input[in_])) {
+                char_array_4[i++] = input[in_]; in_++;
+                if (i ==4) {
+                    for (i = 0; i <4; i++)
+                        char_array_4[i] = Private::Base64Chars.find(char_array_4[i]);
+                    
+                    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+                    char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+                    char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+                    
+                    for (i = 0; (i < 3); i++)
+                        ret += char_array_3[i];
+                    i = 0;
+                }
+            }
+            
+            if (i) {
+                for (j = i; j <4; j++)
+                    char_array_4[j] = 0;
+                
+                for (j = 0; j <4; j++)
+                    char_array_4[j] = Private::Base64Chars.find(char_array_4[j]);
+                
                 char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
                 char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
                 char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-                for (i = 0; (i < 3); i++)
-                    ret += char_array_3[i];
-                i = 0;
+                
+                for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
             }
+            
+            return ret;
         }
-
-        if (i) {
-            for (j = i; j <4; j++)
-            char_array_4[j] = 0;
-
-            for (j = 0; j <4; j++)
-                char_array_4[j] = Private::Base64Chars.find(char_array_4[j]);
-
-            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-            for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
-        }
-
-        return ret;
-    }
-
-} // namespace Base64
+    };
 
 NIMBLE_END
 
